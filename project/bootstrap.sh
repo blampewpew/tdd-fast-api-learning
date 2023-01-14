fb082e343ecad688ed046880c0ab54bc79caecc8
Bryan Lam <blam@ebsco.com>
2023-01-14 14:48:39 -0700

    Comment out aerich

--- project/bootstrap.sh
+++ project/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-echo "Running migrations..."
-aerich upgrade
+#echo "Running migrations..."
+#aerich upgrade
 
 echo "Running gunicorn..."
 gunicorn --bind 0.0.0.0:$PORT app.main:app -k uvicorn.workers.UvicornWorker
